617018f41079f815acd6012d6581a6024bae72f1
kellybarber <kelly.james.barber@gmail.com>
2018-04-07 14:33:35 -0400

    Set up redux store and connected app to it

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers'
+import Game from './containers/Game'
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+  reducer
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Game/>
+  </Provider>, 
+  document.getElementById('root')
+)
+
+registerServiceWorker()
